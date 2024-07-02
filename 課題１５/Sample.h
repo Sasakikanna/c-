d9d46f87b031fbb5ada56e5edc2fa2282ced09b4
栞成 佐々木 <230363@jc-21.jp>
2024-07-02 12:20:54 +0900

    課題１５終わり

---
+++ 課題１５/Sample.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<stdio.h>
+#include<iostream>
+
+class Sample {
+public:
+	//ƒƒ“ƒo•Ï”‚ğì‚é(‚¾‚¯)
+	int a;
+	int b;
+	int c;
+
+	//ƒƒ“ƒoŠÖ”‚ğì‚é(‚¾‚¯)
+public:
+	void Input();
+	void Plus();
+	void Disp();
+};
---
+++ 課題１５/Sub.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Sample.h"
+//サンプルクラスを継承したクラスを宣言
+class Sub : public Sample
+{
+	//メンバ関数
+public:
+	void Minus();
+};
