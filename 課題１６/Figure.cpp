0376486f0c105b362ed16d3c0254a1dcd2030661
栞成 佐々木 <230363@jc-21.jp>
2024-07-09 12:14:26 +0900

    課題１６終了

---
+++ 課題１６/Figure.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Figure.h"
+
+using namespace std;
+
+void Figure::SetTeihen(float f)
+{
+	teihen = f;
+}
+
+void Figure::SetTakasa(float f)
+{
+	takasa = f;
+}
+
+void Figure::Disp()
+{
+	cout << "–ÊÏ‚Í" << menseki << "‚Å‚·" << endl;
+}
